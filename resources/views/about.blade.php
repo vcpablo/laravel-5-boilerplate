324dbf7aad5d6ea1feeaaa60edbbcf042f179f9e
Pablo Veiga <vcpablo@gmail.com>
2017-09-05 18:00:46 -0300

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+
+@section('content')
+<h1>About</h1>
+@stop
---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel</title>
+</head>
+<body>
+
+@include('header')
+
+@yield('content')
+
+@include('footer')
+</body>
+</html>
